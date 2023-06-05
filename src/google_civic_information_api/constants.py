71b3514fd97edbd53fc09a9d91860e992b546354
Matthew Mathur <matthew.eric.dunn@gmail.com>
2023-06-05 16:44:18 -0400

    Add divisions and elections implementations

---
+++ src/google_civic_information_api/constants.py
@@ -0,0 +1,5 @@
+"""Keep package wide constants"""
+
+DEFAULT_TIMEOUT = 300
+
+BASE_URL = "https://www.googleapis.com/civicinfo/v2"
--- src/google_civic_information_api/representatives.py
+++ src/google_civic_information_api/representatives.py
@@ -1,10 +1,9 @@
 """The representatives module contains functions for the representatives resource."""
 
 import requests
+import constants
 
-DEFAULT_TIMEOUT = 300
-
-BASE_URL = "https://www.googleapis.com/civicinfo/v2/representatives"
+REPS_URL = f"{constants.BASE_URL}/representatives"
 
 VALID_LEVELS = ["administrativeArea1", "administrativeArea2", "country",
                 "international", "locality", "regional", "special", "subLocality1", "subLocality2"]
@@ -28,8 +27,8 @@ def representative_info_by_address(api_key, address, include_offices=True, level
     elif roles and roles not in VALID_ROLES:
         raise ValueError(f"roles must be one of {VALID_ROLES}")
 
-    api_response = requests.get(
-        BASE_URL, params=query_params, timeout=DEFAULT_TIMEOUT)
+    api_response = requests.get(constants.BASE_URL, params=query_params,
+                                timeout=constants.DEFAULT_TIMEOUT)
     return api_response
 
 
@@ -46,6 +45,6 @@ def representative_info_by_division(api_key, ocd_id, recursive=True, levels=None
     elif roles and roles not in VALID_ROLES:
         raise ValueError(f"roles must be one of {VALID_ROLES}")
 
-    api_response = requests.get(f"{BASE_URL}/{ocd_id}", params=query_params,
-                                timeout=DEFAULT_TIMEOUT)
+    api_response = requests.get(f"{constants.BASE_URL}/{ocd_id}", params=query_params,
+                                timeout=constants.DEFAULT_TIMEOUT)
     return api_response
